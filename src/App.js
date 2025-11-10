575acca6153aee56ba3e5dabe8bd30435ec3df39
Jalen Patel <jalen.p@wustl.edu>
2025-11-10 11:07:51 -0600

    Moved state in live in Game component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function Square( {value, onSquareClick} ) {
   );
 }
 
-function Board() {
+function Board({ xIsNext, squares, onPlay }) {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
@@ -23,8 +23,7 @@ function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -79,6 +78,15 @@ function calculateWinner(squares) {
 }
 
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
   return (
     <div className="game">
       <div className="game-board">
